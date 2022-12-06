1bcb5cd40be682d2ea5e1a04974b50cbadd56a7f
yuuri-aqua <116873635+yuuri-aqua@users.noreply.github.com>
2022-12-06 17:13:48 +0900

    Userモデルの単体テストコード作成

---
+++ spec/factories/users.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :user do
+    nickname              { Faker::Name.initials(number: 2) }
+    email                 { Faker::Internet.free_email }
+    password              { Faker::Lorem.characters(number: 6, min_alpha: 1, min_numeric: 1) }
+    password_confirmation { password }
+  end
+end
