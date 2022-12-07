103a8fe84d716a304c4864be30e72b7edea97b23
yuuri-aqua <116873635+yuuri-aqua@users.noreply.github.com>
2022-12-07 10:55:45 +0900

    postsテーブル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :posts
+
   validates :nickname,         presence: true
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
