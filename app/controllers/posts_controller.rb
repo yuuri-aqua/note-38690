364c596a3359844db6e72b1ec3de0761bcdfecd2
yuuri-aqua <116873635+yuuri-aqua@users.noreply.github.com>
2022-12-09 14:45:47 +0900

    ログイン状態保持でマイページに直接遷移

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class  PostsController < ApplicationController
+
   def index
     @user = current_user
     @posts = @user.posts.order(id: "DESC")
@@ -12,4 +13,5 @@ class  PostsController < ApplicationController
   def post_params
     params.permit(:title, :text, :image).merge(user_id: current_user.id)
   end
+
 end
