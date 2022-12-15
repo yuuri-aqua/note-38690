647b491741f912b0d8e195295670e0314bb09169
yuuri-aqua <116873635+yuuri-aqua@users.noreply.github.com>
2022-12-15 10:38:16 +0900

    全体修正１

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#index'
 
-  resources :posts, only: [:index, :new, :create, :edit, :update]
+  resources :posts, only: [:index, :new, :create]
   resources :categories, only: [:new, :create]
 end
