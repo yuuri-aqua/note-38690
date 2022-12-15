1e1fc64048f39602a194bfbe15399200b52fa163
yuuri-aqua <116873635+yuuri-aqua@users.noreply.github.com>
2022-12-15 13:29:20 +0900

    ブックマーク機能実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,13 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_one_attached :image
-  belongs_to :category, optional: true
+  has_one_attached :image, dependent: :destroy
+  belongs_to :category
   has_many :post_tag_relations
   has_many :tags, through: :post_tag_relations
+  has_many :bookmarks, dependent: :destroy
+
+  def bookmarked_by?(user)
+    bookmarks.where(user_id: user).exists?
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#index'
 
-  resources :posts, only: [:index, :new, :create]
+  resources :posts, only: [:index, :new, :create, :show] do
+    resource :bookmarks, only: [:create, :destroy]
+  end
   resources :categories, only: [:new, :create]
+
 end
