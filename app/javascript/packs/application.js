4d1cc2ee21c9ddc7e0c8f833748af6d5429ff1be
yuuri-aqua <116873635+yuuri-aqua@users.noreply.github.com>
2022-12-18 15:20:39 +0900

    デプロイ再試行

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,6 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("../grid_image.js.cofee") 
-//= require masonry/jquery.masonry
 
 require('jquery')
 
